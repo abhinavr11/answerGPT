dfec9fc706ecb085de7b55348636ed02cc46fa17
abhinavr11 <abhinav.raghuvanshi2.ar@gmail.com>
2023-04-13 04:51:53 +0530

    new app

--- streamlit.py
+++ streamlit.py
@@ -8,22 +8,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 from nltk import sent_tokenize
 
 from sentence_transformers import SentenceTransformer
-import os
+import os,time,glob
 import openai
 
 
-@st.cache_data
-def return_docs():
-    file1 = open('WhatsApp Chat with London Wale 💂.txt', 'r',encoding="utf8")
-    Lines = file1.readlines()
+# @st.cache_data
 
-    Lines_new=[]
-    for line in Lines:
-        if line != "\n" and len(line)>4:
-            Lines_new.append(line.replace("\n",""))
-    return Lines_new
 
-documents = return_docs()
+
 
 @st.cache_resource
 def return_model():
@@ -31,8 +23,8 @@ def return_model():
 
 model_emb = return_model()
 
+
 def generate_context(query, vector_embeddings, k):
-    # Although it is not explicitly stated in the official document of sentence transformer, the original BERT is meant for a shorter sentence. We will feed the model by sentences instead of the whole documents.
     sentences = sent_tokenize(query)
     base_embeddings_sentences = model_emb.encode(sentences)
     base_embeddings = np.mean(np.array(base_embeddings_sentences), axis=0)
@@ -53,7 +45,39 @@ def generate_context(query, vector_embeddings, k):
     print("Lines in the context: ", len(final_doc.split("\n")))
     return final_doc
 
-vector_data= np.load('embeddings.npy')
+def getDocs(filename):
+    file1 = open(filename, 'r',encoding="mbcs")
+    Lines = file1.readlines()
+    Lines_new=[]
+    for line in Lines:
+        if line != "\n" and len(line)>4:
+            Lines_new.append(line.replace("\n",""))
+    del Lines
+    return Lines_new
+
+def getEmbs(document,name):
+    
+    if [f for f in glob.glob(f"embs_{name}.npy")] :
+        vector_data= np.load(f'embs_{name}.npy')
+        return vector_data
+    
+    vectors = []
+    with st.spinner(text="Processing the chats hang in there ..."):
+        my_bar = st.progress(0)
+        for i, document in enumerate(documents):
+
+            sentences = sent_tokenize(document)
+            embeddings_sentences = model_emb.encode(sentences)
+            embeddings = np.mean(np.array(embeddings_sentences), axis=0)
+
+            vectors.append(embeddings)
+            my_bar.progress(i/len(documents))
+    
+    with open(f'embs_{name}.npy', 'wb') as f:
+        np.save(f, np.array(vectors))
+    return vectors
+
+
 
 def GPT_Completion(texts):
     ## Call the API key under your account (in a secure way)
@@ -61,7 +85,7 @@ def GPT_Completion(texts):
     response = openai.Completion.create(
     engine="text-davinci-003",
     prompt =  texts,
-    temperature = 0.6,
+    temperature = 0,
     top_p = 1,
     max_tokens = 100,
     frequency_penalty = 0,
@@ -69,36 +93,58 @@ def GPT_Completion(texts):
     )
     return response.choices[0].text
 
-
-
-
-st.title("London Vale")
-
-if "history" not in st.session_state:
-    st.session_state.history = []
-
-
-def generate_answer():
+def generate_answer(name_of_person,vector_data):
     
     user_message = st.session_state.input_text
-    # msg = {'message': user_message }
-    # msg  = json.dumps(msg)
-    # x = requests.post(url, data = msg)
-    
-    # message_bot = x.json()['results']
-    query =  user_message  #"Where can I get good Pizza in London?"
-    print(query,"\n \n \n")
-    context= generate_context(query, vector_data, 20)
-    print(context, "\n\n\n")
-    recipe = f'Given a Whatsapp group chat data as "Chat History" and a query as "Query", output the answer to the query \n Chat History: {context} \n Query: {query} \n Output:'
+    query =  user_message  
+    #print(query,"\n \n \n")
+    context= generate_context(query, vector_data, 15)
+    #print(context, "\n\n\n")
+    recipe = f'Given a Whatsapp chat data as "Chat History" reply in the style of the person named {name_of_person} in the chat \n Chat History: {context} \n Query: {query} \n Output:'
     message_bot = GPT_Completion(recipe)
     st.session_state.history.append({"message": user_message, "is_user": True})
     st.session_state.history.append({"message": message_bot, "is_user": False})
 
+def getUploadedFile():
+    st.session_state.uploaded_file = st.file_uploader("Choose a file",key="file_upload") 
+    if uploaded_file:
+        return uploaded_file
 
-st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
 
-for chat in st.session_state.history:
-    st_message(**chat)  # unpacking
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+if 'ctr' not in st.session_state:
+    st.session_state['ctr'] = 0
+
+
+
+def uploader_callback():
+    if st.session_state['file_uploader'] is not None:
+        st.session_state['ctr'] += 1
+        print('Uploaded file #%d' % st.session_state['ctr'])
+    
+uploaded_file = st.file_uploader(label="File uploader", on_change=uploader_callback, key="file_uploader")
+#uploaded_file = st.file_uploader("Choose a file",key="file_upload")#getUploadedFile()   
+if uploaded_file:
+    documents = getDocs(uploaded_file.name)
+    vector_data = getEmbs(document=documents,name=uploaded_file.name) 
+       
+    print('done')
+    name_of_person = st.text_input(
+    "Enter the name of the person you want the bot to talk like 👇",
+    )  
+    name_of_person = 'Dad'
+  
+    
+    st.text_input("Talk to the bot", key="input_text", on_change=generate_answer,args=(name_of_person,vector_data))
+    for chat in st.session_state.history:
+        st_message(**chat) 
+
+
+
+
+
+
 
 
